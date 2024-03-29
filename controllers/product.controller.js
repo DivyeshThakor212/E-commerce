bb093182f95541b9c94cfad26a2ee1833f47ac0e
DivyeshThakor212 <divyesht212.com>
2024-03-29 14:11:01 +0530

    crud operation in product

---
+++ controllers/product.controller.js
@@ -0,0 +1,74 @@
+const productModel = require("../models/product.model")
+
+//create
+exports.createProduct = async(req,res) => {
+   try {
+    const product = await productModel.create(req.body)
+    return res.status(200).send({
+        succes:true,
+        product
+    })
+   } catch (error) {
+    console.log(error)
+   }
+}
+
+//read
+exports.getproduct = async(req,res) => {
+    try {
+        const allproduct = await productModel.find(req.body)
+    return res.status(200).send({
+        succes:true,
+        allproduct
+    })
+    } catch (error) {
+       console.log(error) 
+       return res.status(500).send({
+        succes:false,
+        message:"internal serer"
+       })
+    }
+}
+
+//update
+exports.updateprduct = async(req,res) => {
+   try {
+    let product = await productModel.findById(req.params.id)
+
+    if(!product){
+        return res.status(200).json({ succes:false, message:"please provide correct id"})
+    }
+    
+    product = await productModel.findByIdAndUpdate(req.params.id, req.body,{ new: true})
+    return res.status(200).json({
+        succes:true,
+        message:"product updated succesfully",
+        product
+       
+    })
+   } catch (error) {
+    console.log(error)
+    return res.status(500).send({ status: false, message: "data not found" })
+   }
+}
+
+//Delete
+exports.deleteproduct = async(req,res) => {
+   try {
+    let deleteProduct =  await productModel.findById(req.params.id)
+    if(!deleteProduct){
+        res.status(200).json({ succes:false, message:"please provide correct id" })
+    }
+
+    deleteProduct = await productModel.findByIdAndDelete(req.params.id)
+    return res.status(200).json({succes:true,message:"deleted succesfully"})
+
+    
+   } catch (error) {
+    console.log(error,"error")
+    return res.status(500).send({
+        status:false,
+        message:"data not deleted "
+    })
+   }
+}
--- models/product.model.js
+++ models/product.model.js
@@ -1,1 +1,32 @@
 const monngose = require("mongoose")
+
+const productSchema = new monngose.Schema({
+    pname:{
+        type:String,
+        require:true,
+        trim:true
+    },
+    category:{
+        type:String,
+        enum:["grocery","clothes","cosmetics","footware"],
+        require:true
+    },
+    price:{
+        type:Number,
+        require:true
+    },
+    description:{
+        type:String,
+        require:true,
+        trim:true
+    },
+    status:{
+        type:String,
+        enum:["available","out of stock"],
+        require:true
+    }
+},{
+    timestamps:true,
+})
+
+module.exports = monngose.model("product",productSchema)
