a02f5923bbd2332e19c502726dd2d17b7282ef23
naisargi053 <--global>
2024-03-29 14:19:53 +0530

    add changes

--- models/product.model.js
+++ models/product.model.js
@@ -1,1 +1,2 @@
 const monngose = require("mongoose")
+
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,12 +1,13 @@
 const express = require("express")
 
-const {registerUser,updateUser,deleteUser,loginuser} = require("../controllers/user.controller")
+const {registerUser,updateUser,deleteUser,loginuser,getUser} = require("../controllers/user.controller")
 
 
 const router = express.Router()
 
 router.route("/register-user").post(registerUser)
 router.route("/login-user").get(loginuser)
+router.route("/get-user").get(getUser)
 router.route("/update-user/:id").get(updateUser)
 router.route("/delete-user/:id").delete(deleteUser)
 
